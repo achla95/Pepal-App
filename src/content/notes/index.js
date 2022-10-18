279578f04993a06d1d62cd024dfa7a5935530025
achla95 <achla95@protonmail.com>
2022-10-18 18:52:45 +0200

    add avg function

--- src/content/notes/index.js
+++ src/content/notes/index.js
@@ -1,22 +1,49 @@
 import {store} from '../../main.js';
 const { invoke } = window.__TAURI__.tauri;
 
+const replaceToLong = (element,len) => {
+    if (len > element.lenght){
+        return element;
+    }
+    return element.substring(0,len) + "...";
+}
+const moyenneGenerale = (sum,len) => {
+    if (len === 0) {
+        return 0;
+    }
+    return sum/len;
+}
 let username = await store.get('username');
 let password = await store.get('password');
 const notesEl = document.querySelector("#notes");
 const room = await invoke("get_notes", {username : username.value, password : password.value}); 
 
+let sum = 0;
+let len = 0;
 const elTbody = document.querySelector("tbody");
 room.forEach((element) => {
     const elTr = document.createElement("tr");
     elTbody.appendChild(elTr);
-    const eltd1 = document.createElement("td");
-    const eltd2 = document.createElement("td");
-    const eltd3 = document.createElement("td");
-    eltd1.textContent = element[0];
-    eltd2.textContent = element[1];
-    eltd3.textContent = element[2];
-    elTr.appendChild(eltd1);
-    elTr.appendChild(eltd2);
-    elTr.appendChild(eltd3);
+    const elDateTd = document.createElement("td");
+    const elMatiereTD = document.createElement("td");
+    const elNoteTd = document.createElement("td");
+    elDateTd.textContent = element[0];
+    elMatiereTD.textContent = replaceToLong(element[1],10);
+    elNoteTd.textContent = element[2];
+    sum += element[2];
+    len+=1;
+    elTr.appendChild(elDateTd);
+    elTr.appendChild(elMatiereTD);
+    elTr.appendChild(elNoteTd);
 });
+
+const elMgTr = document.createElement("tr");
+elTbody.appendChild(elMgTr);
+const elTtileTd = document.createElement("td");
+elTtileTd.textContent = "Moyenne Générale";
+const elEmptyTd = document.createElement("td");
+const elMgTd = document.createElement("td");
+elMgTd.textContent = moyenneGenerale(sum,len);
+elMgTr.appendChild(elTtileTd);
+elMgTr.appendChild(elEmptyTd);
+elMgTr.appendChild(elMgTd);
