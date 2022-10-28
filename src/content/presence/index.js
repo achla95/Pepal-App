94107bff5838efb05c0d7f2fe048ced98ede76c4
achla95 <achla95@protonmail.com>
2022-10-28 19:15:34 +0200

    improve the code.

--- src/content/presence/index.js
+++ src/content/presence/index.js
@@ -1,10 +1,9 @@
 import {store} from '../../main.js';
 const { invoke } = window.__TAURI__.tauri;
 
-let username = await store.get('username');
-let password = await store.get('password');
+const cookie = await store.get('cookie');
 const roomEl = document.querySelectorAll(".salle");
-const room = await invoke("get_room", {username : username.value, password : password.value});
+const room = await invoke("get_room", {cookie : cookie.value});
 
 roomEl.forEach((el) =>{
     el.textContent = room;
