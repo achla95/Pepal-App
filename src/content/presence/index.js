3ce6011b1730605db22263bcae9b672b988702e6
achla95 <achla95@protonmail.com>
2022-11-08 19:16:20 +0100

    posibility to set presence

--- src/content/presence/index.js
+++ src/content/presence/index.js
@@ -8,3 +8,40 @@ const room = await invoke("get_room", {cookie : cookie.value});
 roomEl.forEach((el) =>{
     el.textContent = room;
 })
+
+const checkIfCourse = () => {
+    if (room === "Pas de cours !") {
+        return false;
+    }
+    return true;
+}
+
+
+let morning = document.querySelector("#matin");
+let noon = document.querySelector("#midi");
+
+if (morning) {
+    morning.addEventListener("click", async () => {
+        if (checkIfCourse()) {
+            let presence = await invoke("presence", {cookie : cookie.value});
+            console.log("presence set");    
+        }else {
+            console.log("pas de cours");
+        }
+
+    });
+}
+if (noon) {
+    noon.addEventListener("click", async () => {
+        if (checkIfCourse()) {
+            let presence = await invoke("presence", {cookie : cookie.value});
+            console.log("presence set");
+        }else {
+            console.log("pas de cours");
+        }
+
+    });
+}
+
+
+
